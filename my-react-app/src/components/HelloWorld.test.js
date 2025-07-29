1eb74fe0afc6fafded1f466f9c4c34ecd72a10a0
Shaurya Seth <shauryaseth024@gmail.com>
2025-07-29 12:05:05 +1000

    made some changes to test file

--- my-react-app/src/components/HelloWorld.test.js
+++ my-react-app/src/components/HelloWorld.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import HelloWorld from './HelloWorld';
+import HelloWorld from './HelloWorld'; 
+import '@testing-library/jest-dom';
 
 test('renders hello with Focus Bear', () => {
   render(<HelloWorld name="Focus Bear" />);
   const heading = screen.getByText(/Hello, Focus Bear!/i);
   expect(heading).toBeInTheDocument();
 });
- 
